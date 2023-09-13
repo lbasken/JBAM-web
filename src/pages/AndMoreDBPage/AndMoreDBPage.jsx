3ecd6845fc34f96ce6ef5d83652633bcf2590620
Lorelei <lorelei@basken.com>
2023-09-13 14:26:18 -0400

    added loading wheel to database pages while content is received from Firestore

--- src/pages/AndMoreDBPage/AndMoreDBPage.jsx
+++ src/pages/AndMoreDBPage/AndMoreDBPage.jsx
@@ -1,17 +1,28 @@
 import React, {useEffect, useState} from "react";
 import MediaCard from "../../MediaCard.jsx";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function AndMoreDBPage() {
 
     const [andmore, setAndMore] = useState([]);
+    const [show, setShow] = useState(true);
 
     useEffect(() => {
         fetch("https://api-7xmcazxjaa-uc.a.run.app/and-more")
             .then(response => response.json())
             .then(items => {
                 setAndMore(items);
+                setShow(false);
             });
     }, []);
+
+    if(show === true){
+        return (
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress />
+            </Box>
+        );    }
 
     return <div>
         <h3>... And More</h3>
--- src/pages/BirdDBPage/BirdDBPage.jsx
+++ src/pages/BirdDBPage/BirdDBPage.jsx
@@ -1,20 +1,29 @@
 import React, {useEffect, useState} from "react";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 import MediaCard from "/src/MediaCard.jsx";
 import "./BirdDBPage.css";
 
 export default function BirdDBPage() {
 
     const [birds, setBirds] = useState([]);
-
+    const [show, setShow] = useState(true);
 
     useEffect(() => {
         fetch("https://api-7xmcazxjaa-uc.a.run.app/bird")
             .then(response => response.json())
             .then(items => {
                 setBirds(items);
+                setShow(false);
             });
     }, []);
 
+    if(show === true){
+        return (
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress />
+            </Box>
+        );    }
 
     return <div>
         <h3>Just Birds</h3>
@@ -22,5 +31,4 @@ export default function BirdDBPage() {
             {birds.map(bird => <MediaCard key={`bird_${bird.name}`} image={bird.image} name={bird.name} summary={bird.summary} info= {bird.info} />)}
         </section>
     </div>
-
 }
