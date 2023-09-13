bec8a9a57888c0fca10a106181b0c3989f8ced9a
Lorelei <lorelei@basken.com>
2023-09-12 20:18:05 -0400

    removed unneeded images

--- src/pages/BirdDBPage/BirdDBPage.jsx
+++ src/pages/BirdDBPage/BirdDBPage.jsx
@@ -6,6 +6,7 @@ export default function BirdDBPage() {
 
     const [birds, setBirds] = useState([]);
 
+
     useEffect(() => {
         fetch("https://api-7xmcazxjaa-uc.a.run.app/bird")
             .then(response => response.json())
@@ -14,11 +15,11 @@ export default function BirdDBPage() {
             });
     }, []);
 
+
     return <div>
         <h3>Just Birds</h3>
         <section id="card">
             {birds.map(bird => <MediaCard key={`bird_${bird.name}`} image={bird.image} name={bird.name} summary={bird.summary} info= {bird.info} />)}
-            {/*<MediaCard image="https://www.allaboutbirds.org/guide/assets/photo/302473191-480px.jpg" name="Cute Bird 1" summary="This is a summary." info="https://www.wikipedia.org/" />*/}
         </section>
     </div>
 
